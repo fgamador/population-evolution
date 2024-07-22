a793c3d7bd7f95e22f60f6fe69f3d8b64585eb5f
Franz Amador <franzamador@gmail.com>
2024-07-22 10:10:40 -0700

    Add unit-test scaffolding

---
+++ js/population-evolution-tests.ts
@@ -0,0 +1,13 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * Unit tests for population-evolution.
+ *
+ * @author Franz Amador (open-source contributor)
+ */
+
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+//import './common/view/describers/TickMarkDescriberTests.js';
+//import './common/view/getKeyboardInputSnappingMapperTests.js';
+
+qunitStart();
