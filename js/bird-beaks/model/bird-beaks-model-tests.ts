ae1a1fa852cb803407e2990e973f77706b32db7c
Franz Amador <franzamador@gmail.com>
2024-07-23 16:35:01 -0700

    Format

--- js/bird-beaks/model/bird-beaks-model-tests.ts
+++ js/bird-beaks/model/bird-beaks-model-tests.ts
@@ -16,7 +16,9 @@ QUnit.module( 'Population', function() {
   QUnit.test( 'survivalPhase', function( assert ) {
     var population = new Population( [ new Bird( 0.3 ), new Bird( 0.5 ), new Bird( 0.4 ), new Bird( 0.6 ) ] );
     var rand = new TestRandomSource( [ 0.5 ] );
+
     const [ alive, dead ] = population.survivalPhase( rand, bird => bird.beakSize );
+
     assert.equal( JSON.stringify( alive ), '[{"beakSize":0.5},{"beakSize":0.6}]' );
     assert.equal( JSON.stringify( dead ), '[{"beakSize":0.3},{"beakSize":0.4}]' );
     assert.equal( JSON.stringify( population.birds ), JSON.stringify( alive ) );
