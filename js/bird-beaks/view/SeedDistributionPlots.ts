1ca72e49ad3d6ef0527b332a14436d5d60c82f95
Franz Amador <franzamador@gmail.com>
2024-08-14 15:17:16 -0700

    Draw both seed distribution plots

--- js/bird-beaks/view/SeedDistributionPlots.ts
+++ js/bird-beaks/view/SeedDistributionPlots.ts
@@ -54,7 +54,7 @@ export default class SeedDistributionPlots extends Node {
       viewWidth: options.diagramWidth,
       viewHeight: options.diagramHeight,
       modelXRange: new Range( options.minValue, options.maxValue ),
-      modelYRange: new Range( 0, 12 ) // todo max height
+      modelYRange: new Range( 0, 10 ) // todo max height
     } );
 
     const chartRectangle = new ChartRectangle( chartTransform, {
@@ -76,7 +76,8 @@ export default class SeedDistributionPlots extends Node {
           // Minor grid lines
           new GridLineSet( chartTransform, Orientation.VERTICAL, majorYTickSpacing * 2, { stroke: 'lightGray' } ),
 
-          new LinePlot( chartTransform, toPlotDataSet( seeds.distributions[ 0 ] ), { stroke: 'red', lineWidth: 2 } )
+          new LinePlot( chartTransform, toPlotDataSet( seeds.distributions[ 0 ] ), { stroke: 'red', lineWidth: 2 } ),
+          new LinePlot( chartTransform, toPlotDataSet( seeds.distributions[ 1 ] ), { stroke: 'red', lineWidth: 2 } )
         ]
       } ),
 
