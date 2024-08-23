4be84628c4be29783f323fc07bc6451f5869ab25
Franz Amador <franzamador@gmail.com>
2024-08-23 16:27:38 -0700

    Extract function

--- js/common/view/TickSpacing.ts
+++ js/common/view/TickSpacing.ts
@@ -13,23 +13,26 @@ export default class TickSpacing {
   public static pleasingMinorTickSpacing( range: number, optimalTickCount: number ): number {
     const optimalSpacing = range / optimalTickCount;
     const optimalSpacingOOM = this.orderOfMagnitude( optimalSpacing );
-    const ratio = optimalSpacing / optimalSpacingOOM;
-
-    if ( ratio >= 7.5 ) {
-      return optimalSpacingOOM * 10;
-    }
-    if ( ratio >= 3.5 ) {
-      return optimalSpacingOOM * 5;
-    }
-    if ( ratio >= 1.5 ) {
-      return optimalSpacingOOM * 2;
-    }
-
-    return optimalSpacingOOM;
+    return optimalSpacingOOM * this.minorTickSpacingOOMMultiplier( optimalSpacing / optimalSpacingOOM );
   }
 
   public static orderOfMagnitude( x: number ): number {
     return Math.pow( 10, Math.floor( Math.log10( x ) ) );
+  }
+
+  private static minorTickSpacingOOMMultiplier( optimalSpacingToOOMRatio: number ): number {
+    if ( optimalSpacingToOOMRatio >= 7.5 ) {
+      return 10;
+    }
+    else if ( optimalSpacingToOOMRatio >= 3.5 ) {
+      return 5;
+    }
+    else if ( optimalSpacingToOOMRatio >= 1.5 ) {
+      return 2;
+    }
+    else {
+      return 1;
+    }
   }
 
   public static pleasingMajorTickSpacing( range: number, optimalTickCount: number ): number {
